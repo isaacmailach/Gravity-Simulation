b78a074d33150da6f662582b43524a6e4021b2be
Isaac Mailach <isaac.mailach@gmail.com>
2015-12-31 18:30:23 -0500

    Initial ball drag implementation
    Enable ball drag mechanic. Set up ball drag line drawing. Ball colour
    dependent on ball density. Add units to properties menu.

--- js/definitions.js
+++ js/definitions.js
@@ -2,30 +2,46 @@ var canvas = new Object();
 var window = new Object();
 
 var properties = {
-  "d": 5.514 * Math.pow(10, 12),
+  "d": 5514,
   "framerate": 60,
   //"paused": false,
+  "pull-strength": 0.20,
   "r": 6371,
   "scale": Math.pow(10, -2),
-  "speed": 100
+  "speed": 1000
 };
 
 var balls = new Array();
 
-function Ball(colour, d, r, x, y) {
+function Ball(d, r, vx, vy, x, y) {
     this.a = new Object();
     this.a.x = 0;
     this.a.y = 0;
-    this.colour = colour;
-    this.d = d;
+    var num = Math.ceil(parseInt((1 - d / 10000) * 255));
+    var hex = null;
+    if (num <= 15) {
+      hex = '0' + num.toString(16);
+    } else {
+      hex = num.toString(16);
+    }
+    this.colour = '#' + hex + "0000";
+    this.d = d * Math.pow(10, 9);
     this.f = new Object();
     this.f.x = 0;
     this.f.y = 0;
-    this.m = 4 / 3 * Math.pow(r, 3) * Math.PI * d;
+    this.m = 4 / 3 * Math.pow(r, 3) * Math.PI * d * Math.pow(10, 9);
     this.v = new Object();
-    this.v.x = 0;
-    this.v.y = 0;
+    this.v.x = vx;
+    this.v.y = vy;
     this.r = r;
     this.x = x;
     this.y = y;
+}
+
+var dragline = {
+  "drag": false,
+  "startx": null,
+  "starty": null,
+  "currentx": null,
+  "currenty": null
 }
--- js/interactions.js
+++ js/interactions.js
@@ -1,8 +1,31 @@
 $(document).ready(function() {
-  $('#canvas').click(function(e) {
+  $('#canvas').on('mousedown', function(e) {
     var left = $(this).offset().left;
     var top = $(this).offset().top;
-    balls.push(new Ball('red', properties.d, properties.r, e.pageX - left, e.pageY - top));
+    var startx = e.pageX;
+    var starty = e.pageY;
+    $('#canvas').on('mouseup mousemove', function handler(e) {
+      if (e.type === 'mouseup') {
+        balls.push(new Ball(properties.d, properties.r, 0, 0, startx - left, starty - top));
+      } else if (e.type === 'mousemove'){
+        $('#canvas').on('mouseup', function ehandler(e) {
+          var endx = e.pageX;
+          var endy = e.pageY;
+          var vx = (endx - startx) * properties["pull-strength"];
+          var vy = (endy - starty) * properties["pull-strength"];
+          balls.push(new Ball(properties.d, properties.r, vx, vy, startx - left, starty - top));
+          $('#canvas').off('mouseup', ehandler);
+        });
+      }
+      $('#canvas').off('mouseup mousemove', handler);
+    });
+    $('#canvas').on('mousemove', function(e) {
+      dragline.drag = true;
+      dragline.startx = startx;
+      dragline.starty = starty;
+      dragline.currentx = e.pageX;
+      dragline.currenty = e.pageY;
+    });
   });
 
   $('#menu_open').click(function() {
